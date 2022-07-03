7fc5b35fa278522919dbd1ae1156bf279a1c032c
GoKyoungEun <seven2649@naver.com>
2022-07-03 13:52:46 +0900

    220703 수정

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-let str = `
-010-1234-5678.
-the1234@gmail.com
+const str = `
+010-1234-5678
+thesecon@gmail.com
 http://www.omdbapi.com/?apikey=7035c60c&s=frozen
 The quick brown fox jumps over the lazy dog.
 aabbccdddd
 `
 
-// .은 js에서 특정한 기능으로 작동하기 때문에 
-// ＼(백슬래시)기호를 넣어 일반적인 문자로 해석되게 하기위에 앞에 넣는다.
-// $ - 줄(Line)의 끝에서 일치, /xyz$/
-console.log(str.match(/\.$/gi))
+console.log(
+  // 한 개 이상 반복되는 임의의 문자를 @기준 앞쪽으로 일치시킨다.
+  str.match(/(?<=@).{1,}/g)
+)
